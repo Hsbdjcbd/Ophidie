cb997ead4578a97f1b6f441b06d410a22b61b2e8
Vincent Gagnon <vincedeark@gmail.com>
2026-04-16 10:50:09 -0400

    setting up Settings.h

--- Settings.h
+++ Settings.h
@@ -1,5 +1,43 @@
+/*====================================
+Auteur : Vincent Gagnon
+Projet : Ophidie
+====================================*/
+
 #pragma once
+
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
 class Settings
 {
+private:
+
+public:
+
 };
 
+/*
+TODO:
+- volume musique
+- volume sound
+- grid size
+- language
+- difficulty
+	- Longueur snake
+	- Vie
+	- Vitesse
+	- Egg point
+- full screen mode
+- Mapping des touches (wasd vs arrow key)
+
+secondary:
+- background
+- skins
+- master volume
+- Mapping des touches (full mapping)
+- timer before starving
+
+*/
