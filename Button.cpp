7feedcb80c5f060209928ac97ef5e59bd8d5f21a
Jérémy Hébert <jeray.gaming@gmail.com>
2026-04-23 10:26:48 -0400

    Création de la class button

--- Button.cpp
+++ Button.cpp
@@ -1,1 +1,74 @@
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <cassert>
+
+#include "const.h"
 #include "Button.h"
+
+// Constructeurs
+Button::Button()
+{
+	_scale = _position;
+	_buttonID = _isMouseReleased = 0;
+}
+
+Button::Button(const sf::RectangleShape rectangle, const sf::Text text, const sf::Color fillColor, const sf::Color outlineColor, const sf::Vector2f scale, const sf::Vector2f position, const unsigned int _buttonID)
+{
+	setButtonID
+}
+
+// Destructeur
+Button::~Button()
+{
+	_scale = _position;
+	_buttonID = _isMouseReleased = 0;
+}
+
+// Getters
+sf::RectangleShape Button::getRectangle() const
+{
+	return _rectangle;
+}
+
+sf::Text Button::getText() const
+{
+	return _text;
+}
+
+sf::Color Button::getFillColor() const
+{
+	return _fillColor;
+}
+
+sf::Color Button::getOutlineColor() const
+{
+	return _outlineColor;
+}
+
+sf::Vector2f Button::getScale() const
+{
+	return _scale;
+}
+
+sf::Vector2f Button::getPosition() const
+{
+	return _position;
+}
+
+unsigned int Button::getButtonID() const
+{
+	return _buttonID;
+}
+
+bool Button::getIsMouseReleased() const
+{
+	return _isMouseReleased;
+}
+
+// Setters
+void Button::setRectangle()
+{
+
+}
+
--- Button.h
+++ Button.h
@@ -1,4 +1,35 @@
 #pragma once
 class Button
 {
+private:
+	sf::RectangleShape _rectangle;
+	sf::Text _text;
+	sf::Color _fillColor, _outlineColor;
+	sf::Vector2f _scale, _position;
+
+	unsigned int _buttonID;
+	bool _isMouseReleased;
+
+public:
+	// Constructeurs
+	Button();
+	Button(const sf::RectangleShape rectangle, const sf::Text text, const sf::Color fillColor, const sf::Color outlineColor, const sf::Vector2f scale, const sf::Vector2f position, const unsigned int _buttonID);
+
+	// Destructeur
+	~Button();
+
+	// Getters
+	sf::RectangleShape getRectangle() const;
+	sf::Text getText() const;
+	sf::Color getFillColor() const;
+	sf::Color getOutlineColor() const;
+	sf::Vector2f getScale() const;
+	sf::Vector2f getPosition() const;
+	unsigned int getButtonID() const;
+	bool getIsMouseReleased() const;
+
+	// Setters
+	void setButtonID();
+	void setIsmouseReleased();
+
 };
