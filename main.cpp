83bf6d7950f3359efff349eeec819d23decfe397
Jérémy Hébert <53281029+JeremyHebertQc@users.noreply.github.com>
2026-04-23 10:59:12 -0400

    Update main.cpp
    Ajout du lien interfichier manquant

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
+/*====================================
+Auteurs : Jérémy Hébert, Vincent Gagnon et Félix-Antoine Lacroix
+Projet : Ophidie
+====================================*/
 #include <iostream>
+#include "const.h"
 
 int main() {
     std::cout << "Hello, world!";
